6603f2a1dbdb8832b6e34ab082e0a2872eeb88d4
chris haynes <r.chris.haynes@gmail.com>
2020-03-01 14:31:24 -0500

    added click event to button

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-        return add(b);
-        }
-    }
+    
 })();
 
-
+// UI CONTROLLER
 var UIController = (function() {
     
     // some code
 
 })();
 
-
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5); 
-
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
-    }
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log('hi')
+    });
 
 })(budgetController,UIController);
